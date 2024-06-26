24eece81af27558fc0125849784a1cf56f6c27a4
Aashutosh Shukla <aashu.guru0@gmail.com>
2024-06-26 22:26:10 +0530

    reduced code

--- DAY14/database/db.js
+++ DAY14/database/db.js
@@ -9,19 +9,10 @@ dbURL=dbURL.replace("<password>", process.env.DB_PASSWORD);
 dbURL=dbURL.replace("<dbName>", process.env.DB_NAME);
 
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(dbURL, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
+const client = new MongoClient(dbURL);
 const database = client.db(process.env.DB_NAME);
-const produtsCollection = database.collection("products");
+const productsCollection = database.collection("products");
 
 module.exports = {
-    database,
-    produtsCollection,
+    productsCollection,
 };
--- DAY14/main.js
+++ DAY14/main.js
@@ -1,7 +1,13 @@
-require('dotenv').config();
-require("./database/db");
-const express = require('express');
+require("dotenv").config();
+
+const express = require("express");
+const productRouter = require("./routes/productRoutes.js");
+
 const app = express();
+app.use(express.json());
+
+app.use("/products", productRouter);
+
 app.listen(process.env.PORT,()=>{
   console.log(`--------App started on: ${process.env.PORT}----------`);
 });
