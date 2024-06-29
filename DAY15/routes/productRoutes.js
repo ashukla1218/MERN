ca3184b20a5b8e266ba90a0147f901e9c83958e0
Aashutosh Shukla <aashu.guru0@gmail.com>
2024-06-29 18:59:27 +0530

    Day17 Advance query

--- DAY15/routes/productRoutes.js
+++ DAY15/routes/productRoutes.js
@@ -1,11 +1,11 @@
 const express = require ("express");
-const { getProducts, createProduct, replaceProduct, checkId, updateProduct,deleteProduct,listProduct } = require("../controllers/productControllers");
+const { getProducts, createProduct, replaceProduct, checkId, updateProduct,deleteProduct,listProducts } = require("../controllers/productControllers");
 
 const productRouter = express.Router();
 
 productRouter.route("/").get(getProducts).post(createProduct);
 
-productRouter.route("/list").get(listProduct);
+productRouter.route("/list").get(listProducts);
 
 productRouter.route("/:id").put(checkId,replaceProduct).patch(checkId,updateProduct).delete(deleteProduct);
 
